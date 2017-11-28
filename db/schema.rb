129af82c97474db79ab211fda8f6a4468cb897a2
JayLatt <jlattimor3@gmail.com>
2017-11-28 01:38:37 -0500

    incorporated a name system for signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122022605) do
+ActiveRecord::Schema.define(version: 20171128050201) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20171122022605) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
