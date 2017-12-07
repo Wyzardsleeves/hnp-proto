bbf7ba77c99b4df8d8e2082d6838de0ef2564b6c
JayLatt <jlattimor3@gmail.com>
2017-12-06 20:14:41 -0500

    added some seeding functionality that users won't notice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "random_data"
+
+1.times do
+  Post.create!(
+    title:    Random_Data.random_sentence,
+    content:  Random_Data.random_paragraph
+  )
+end
+posts = Post.all
+
+2.times do
+  Comment.create!(
+    post: posts.sample,
+    comment: Random_Data.random_sentence
+  )
+end
+
+puts "Seeding complete"
+puts "#{Post.count} post created!"
+puts "#{Comment.count} comments created!"
